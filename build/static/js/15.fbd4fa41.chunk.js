e6144f8da8ee0ade20cc7abaa3a882671ad333d1
Anil Loutombam <anil.singha@refixd.com>
2020-10-08 15:45:22 +0530

    added build

--- build/static/js/15.fbd4fa41.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpno-stack-base"]=this["webpackJsonpno-stack-base"]||[]).push([[15],{651:function(t,e,n){"use strict";n.r(e);var a=n(631),c=n(632),r=n(634),s=n(633),i=n(0),o=n.n(i),u=n(661),b=function(t){Object(r.a)(n,t);var e=Object(s.a)(n);function n(){return Object(a.a)(this,n),e.apply(this,arguments)}return Object(c.a)(n,[{key:"render",value:function(){return o.a.createElement(u.a,{width:"100%",height:"485",video:"413548140"}," ")}}]),n}(i.Component);e.default=b}}]);
-//# sourceMappingURL=15.fbd4fa41.chunk.js.map
