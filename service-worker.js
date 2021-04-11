8e9278c41fbfe1a35b44185e317049b2d1a247dc
clark candia <clarkcandia@outlook.com>
2021-04-12 01:35:11 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1fe9c034029792fce7195556aff7c9d2.js"
+  "/precache-manifest.e16ab0d508665eaed620343ef5e12a96.js"
 );
 
 self.addEventListener('message', (event) => {
